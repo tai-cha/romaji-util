9b635b2829fe88362498678e3437718720952e0b
taichanne30 <ne300209@senshu-u.jp>
2021-04-23 09:22:58 +0900

    Add some command

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import Kana from './utils/kana.js'
-class RomajiUtil {
+import Hebon from './utils/romaji/hebon.js'
+export default class RomajiUtil {
     static Kana = Kana
+    static Hebon = Hebon
 }
-export default RomajiUtil
 
-if(process.argv.length > 2) {
-    console.log(RomajiUtil.Kana.toRomaji(process.argv.slice(2).join(' ')))
+if(process.argv.length > 3) {
+    if(process.argv[2] == "romaji") {
+        console.log(RomajiUtil.Kana.toRomaji(process.argv.slice(3).join(' ')).value)
+    } else if (process.argv[2] == "kana") {
+        console.log(RomajiUtil.Hebon.toKana(process.argv.slice(3).join(' ')).value)
+    }
 } else {
     console.error('You cannot excecute this script without argument.')
 }
