ef27a563d3123f012a040b1bbdd2cbe6d89a71d3
taichanne30 <ne300209@senshu-u.jp>
2021-04-28 17:31:27 +0900

    Add sample_page

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ export default class RomajiUtil {
     static Hebon = Hebon
 }
 
-if(process.argv.length > 3) {
-    if(process.argv[2] == "romaji") {
-        console.log(RomajiUtil.Kana.toRomaji(process.argv.slice(3).join(' ')).value)
-    } else if (process.argv[2] == "kana") {
-        console.log(RomajiUtil.Hebon.toKana(process.argv.slice(3).join(' ')).value)
+if (typeof process !== 'undefined'){
+    if(process.argv.length > 3) {
+        if(process.argv[2] == "romaji") {
+            console.log(RomajiUtil.Kana.toRomaji(process.argv.slice(3).join(' ')).value)
+        } else if (process.argv[2] == "kana") {
+            console.log(RomajiUtil.Hebon.toKana(process.argv.slice(3).join(' ')).value)
+        }
+    } else {
+        console.error('You cannot excecute this script without argument.')
     }
-} else {
-    console.error('You cannot excecute this script without argument.')
 }
